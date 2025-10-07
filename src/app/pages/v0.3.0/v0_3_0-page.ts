e4726dcb56406c9eabc800f4cb47926a88c7ac2c
Boris Vasilev <bvasilev@uwo.ca>
2025-10-07 15:37:10 -0400

    Refactor code examples for better readability and organization

--- src/app/pages/v0.3.0/v0_3_0-page.ts
+++ src/app/pages/v0.3.0/v0_3_0-page.ts
@@ -5,21 +5,24 @@ import {RouterOutlet} from '@angular/router';
 import {
   SidebarElement, SidebarLink, SidebarRedirect,
 } from '../../../sidebar-element';
-import {SyntaxPage} from './syntax/syntax.page';
-import {TypesPage} from './types/types.page';
-import {FunctionsPage} from './functions/functions.page';
-import {ControlFlowPage} from './control-flow/control-flow.page';
+import {SyntaxPage} from './language-guide/syntax/syntax.page';
+import {TypesPage} from './language-guide/types/types.page';
+import {
+  VariablesAndScopePage,
+} from './language-guide/variables-and-scope/variables-and-scope.page';
+import {FunctionsPage} from './language-guide/functions/functions.page';
+import {ControlFlowPage} from './language-guide/control-flow/control-flow.page';
+import {OperatorsPage} from './language-guide/operators/operators.page';
 import {LanguageGuidePage} from './language-guide/language-guide.page';
 import {StdlibPage} from './stdlib/stdlib.page';
-import {PrintPage} from './print/print.page';
-import {PrintLinePage} from './print-line/print-line.page';
-import {PrintErrorPage} from './print-error/print-error.page';
-import {InputPage} from './input/input.page';
+import {PrintPage} from './stdlib/print/print.page';
+import {PrintLinePage} from './stdlib/print-line/print-line.page';
+import {PrintErrorPage} from './stdlib/print-error/print-error.page';
+import {InputPage} from './stdlib/input/input.page';
 
 @Component({
   selector: 'app-v0.3.0.page',
-  imports: [
-    SidebarComponent, RouterOutlet],
+  imports: [SidebarComponent, RouterOutlet],
   templateUrl: '../version.page.html',
   styleUrl: '../version.page.css',
 })
@@ -32,10 +35,19 @@ export class V0_3_0Page extends Version {
       children: [
         {path: '', redirectTo: 'syntax', pathMatch: 'full'} as SidebarRedirect,
         {
-          name: 'Syntax', path: 'syntax', component: SyntaxPage, children: [],
+          name: 'Syntax Overview',
+          path: 'syntax',
+          component: SyntaxPage,
+          children: [],
         } as SidebarLink,
         {
           name: 'Types', path: 'types', component: TypesPage, children: [],
+        } as SidebarLink,
+        {
+          name: 'Variables & Scope',
+          path: 'variables-and-scope',
+          component: VariablesAndScopePage,
+          children: [],
         } as SidebarLink,
         {
           name: 'Functions',
@@ -47,6 +59,12 @@ export class V0_3_0Page extends Version {
           name: 'Control Flow',
           path: 'control-flow',
           component: ControlFlowPage,
+          children: [],
+        } as SidebarLink,
+        {
+          name: 'Operators',
+          path: 'operators',
+          component: OperatorsPage,
           children: [],
         } as SidebarLink],
     } as SidebarLink, {
