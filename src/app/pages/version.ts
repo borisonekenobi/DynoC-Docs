1057d1e4e2fb1422c7634b64fcc297ca2d859dc3
Boris Vasilev <bvasilev@uwo.ca>
2025-10-06 23:20:38 -0400

    Refactor version page classes to extend Version base class

---
+++ src/app/pages/version.ts
@@ -0,0 +1,8 @@
+import {SidebarElement} from '../../sidebar-element';
+
+export class Version {
+  static sidebarLayout: SidebarElement[];
+  available!: boolean;
+  version!: string;
+  sidebarLayout!: SidebarElement[];
+}
