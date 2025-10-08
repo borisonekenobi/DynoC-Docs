476981d414844c74d04fa972c31fdc670b52fde7
Boris Vasilev <bvasilev@uwo.ca>
2025-10-07 22:58:57 -0400

    Enhance control flow documentation with detailed examples and add new components for Break, Continue, and Exit

--- src/app/pages/v0.3.0/language-guide/control-flow/control-flow.page.ts
+++ src/app/pages/v0.3.0/language-guide/control-flow/control-flow.page.ts
@@ -1,8 +1,55 @@
 import {Component} from '@angular/core';
+import {
+  IntComponent,
+} from '../../../../components/code/keyword/int/int.component';
+import {
+  NumberComponent,
+} from '../../../../components/code/number/number.component';
+import {IfComponent} from '../../../../components/code/keyword/if/if.component';
+import {
+  FunctionComponent,
+} from '../../../../components/code/function/function.component';
+import {
+  StringComponent,
+} from '../../../../components/code/string/string.component';
+import {
+  ElseComponent,
+} from '../../../../components/code/keyword/else/else.component';
+import {
+  WhileComponent,
+} from '../../../../components/code/keyword/while/while.component';
+import {
+  ForComponent,
+} from '../../../../components/code/keyword/for/for.component';
+import {
+  BreakComponent,
+} from '../../../../components/code/keyword/break/break.component';
+import {
+  ContinueComponent,
+} from '../../../../components/code/keyword/continue/continue.component';
+import {
+  CommentComponent,
+} from '../../../../components/code/comment/comment.component';
+import {
+  ExitComponent
+} from '../../../../components/code/keyword/exit/exit.component';
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [
+    IntComponent,
+    NumberComponent,
+    IfComponent,
+    FunctionComponent,
+    StringComponent,
+    ElseComponent,
+    WhileComponent,
+    ForComponent,
+    BreakComponent,
+    ContinueComponent,
+    CommentComponent,
+    ExitComponent,
+  ],
   templateUrl: './control-flow.page.html',
   styleUrl: './control-flow.page.css',
 })
