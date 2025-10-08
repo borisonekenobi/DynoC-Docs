9014280749eeed9ad89625f039add76a22ac9acf
Boris Vasilev <bvasilev@uwo.ca>
2025-10-07 22:32:23 -0400

    Enhance Functions documentation with detailed explanations, examples, and improved component imports

--- src/app/pages/v0.3.0/language-guide/functions/functions.page.ts
+++ src/app/pages/v0.3.0/language-guide/functions/functions.page.ts
@@ -1,11 +1,49 @@
 import {Component} from '@angular/core';
+import {
+  VoidComponent,
+} from '../../../../components/code/keyword/void/void.component';
+import {
+  FunctionComponent,
+} from '../../../../components/code/function/function.component';
+import {
+  StringComponent,
+} from '../../../../components/code/string/string.component';
+import {
+  StringKWComponent,
+} from '../../../../components/code/keyword/string/string.component';
+import {
+  IntComponent,
+} from '../../../../components/code/keyword/int/int.component';
+import {
+  NumberComponent,
+} from '../../../../components/code/number/number.component';
+import {
+  CommentComponent,
+} from '../../../../components/code/comment/comment.component';
+import {
+  ReturnComponent
+} from '../../../../components/code/keyword/return/return.component';
+import {IfComponent} from '../../../../components/code/keyword/if/if.component';
+import {
+  FloatComponent
+} from '../../../../components/code/keyword/float/float.component';
 
 @Component({
   selector: 'app-functions',
-  imports: [],
+  imports: [
+    VoidComponent,
+    FunctionComponent,
+    StringComponent,
+    StringKWComponent,
+    IntComponent,
+    NumberComponent,
+    CommentComponent,
+    ReturnComponent,
+    IfComponent,
+    FloatComponent,
+  ],
   templateUrl: './functions.page.html',
   styleUrl: './functions.page.css',
 })
 export class FunctionsPage {
-
 }
