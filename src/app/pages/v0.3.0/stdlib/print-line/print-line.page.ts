078b396f0dccb42160bb4897d5575863c5f7ba16
Boris Vasilev <bvasilev@uwo.ca>
2025-10-08 00:18:32 -0400

    Enhance print_line() documentation with detailed descriptions, examples, and parameter table

--- src/app/pages/v0.3.0/stdlib/print-line/print-line.page.ts
+++ src/app/pages/v0.3.0/stdlib/print-line/print-line.page.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {
+  FunctionComponent,
+} from '../../../../components/code/function/function.component';
 
 @Component({
   selector: 'app-print-line',
-  imports: [],
+  imports: [
+    FunctionComponent],
   templateUrl: './print-line.page.html',
   styleUrl: './print-line.page.css',
 })
