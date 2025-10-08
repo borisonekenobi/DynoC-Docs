d688fc6e69974da24e0cc829ffa8c926318ab111
Boris Vasilev <bvasilev@uwo.ca>
2025-10-07 22:58:41 -0400

    Add Break, Continue, and Exit components with corresponding templates and tests

---
+++ src/app/components/code/keyword/break/break.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {KeywordComponent} from '../keyword.component';
+
+@Component({
+  selector: 'break',
+  imports: [
+    KeywordComponent],
+  templateUrl: './break.component.html',
+  styleUrl: './break.component.css',
+})
+export class BreakComponent {
+}
---
+++ src/app/components/code/keyword/exit/exit.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {KeywordComponent} from '../keyword.component';
+
+@Component({
+  selector: 'exit',
+  imports: [
+    KeywordComponent],
+  templateUrl: './exit.component.html',
+  styleUrl: './exit.component.css',
+})
+export class ExitComponent {
+}
