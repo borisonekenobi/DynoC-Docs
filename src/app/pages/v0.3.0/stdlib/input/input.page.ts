b942df7463bc95025b6cd611869530f29266fafa
Boris Vasilev <bvasilev@uwo.ca>
2025-10-08 00:35:53 -0400

    Enhance input() documentation with detailed descriptions, examples, and parameter table

--- src/app/pages/v0.3.0/stdlib/input/input.page.ts
+++ src/app/pages/v0.3.0/stdlib/input/input.page.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {
+  FunctionComponent,
+} from '../../../../components/code/function/function.component';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [
+    FunctionComponent],
   templateUrl: './input.page.html',
   styleUrl: './input.page.css',
 })
