e6deb1f40f281c59eef64f85333e14f9c0ee55ec
Boris Vasilev <bvasilev@uwo.ca>
2025-10-08 22:25:50 -0400

    Add Input, Print, Print Error, and Print Line components with basic structure and tests

---
+++ src/app/components/code/function/print-error/print-error.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {FunctionComponent} from '../function.component';
+
+@Component({
+  selector: 'print_error',
+  imports: [
+    FunctionComponent],
+  templateUrl: './print-error.component.html',
+  styleUrl: './print-error.component.css',
+})
+export class PrintErrorComponent {
+}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,22 @@
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {ForComponent} from '../../components/code/keyword/for/for.component';
+import {IntComponent} from '../../components/code/keyword/int/int.component';
+import {NumberComponent} from '../../components/code/number/number.component';
+import {
+  PrintLineComponent
+} from '../../components/code/function/print-line/print-line.component';
+import {StringComponent} from '../../components/code/string/string.component';
 
 @Component({
   selector: 'app-home', imports: [
-    RouterLink], templateUrl: './home.page.html', styleUrl: './home.page.css',
+    RouterLink,
+    ForComponent,
+    IntComponent,
+    NumberComponent,
+    PrintLineComponent,
+    StringComponent,
+  ], templateUrl: './home.page.html', styleUrl: './home.page.css',
 })
 export class HomePage {
 
