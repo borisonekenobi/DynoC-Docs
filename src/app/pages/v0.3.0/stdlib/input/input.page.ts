58a65a5c4f9a8c22da79f266752d08e37a30ea61
Boris Vasilev <bvasilev@uwo.ca>
2025-10-11 01:33:30 -0400

    Update links to use routerLink for navigation in HTML templates

--- src/app/pages/v0.3.0/stdlib/input/input.page.ts
+++ src/app/pages/v0.3.0/stdlib/input/input.page.ts
@@ -2,11 +2,13 @@ import {Component} from '@angular/core';
 import {
   InputComponent
 } from '../../../../components/code/function/input/input.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-input',
   imports: [
     InputComponent,
+    RouterLink,
   ],
   templateUrl: './input.page.html',
   styleUrl: './input.page.css',
--- src/app/pages/v0.3.0/stdlib/print-error/print-error.page.ts
+++ src/app/pages/v0.3.0/stdlib/print-error/print-error.page.ts
@@ -2,11 +2,13 @@ import {Component} from '@angular/core';
 import {
   PrintErrorComponent
 } from '../../../../components/code/function/print-error/print-error.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-print-error',
   imports: [
     PrintErrorComponent,
+    RouterLink,
   ],
   templateUrl: './print-error.page.html',
   styleUrl: './print-error.page.css',
