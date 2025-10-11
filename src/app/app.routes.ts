c9738d72ae0e57eb0e1dd4f0ea56cf72f677b7a1
Boris Vasilev <bvasilev@uwo.ca>
2025-10-11 01:24:23 -0400

    Add 404 error handling with WrongRouteComponent and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,29 @@ import {Routes} from '@angular/router';
 import {HomePage} from './pages/home/home.page';
 import {GettingStartedPage} from './pages/getting-started/getting-started.page';
 import {DocumentationPage} from './pages/documentation/documentation.page';
+import {V0_3_1Page} from './pages/v0.3.1/v0_3_1-page';
+import {V0_3_0Page} from './pages/v0.3.0/v0_3_0-page';
 import {ExamplesPage} from './pages/examples/examples.page';
 import {ContributingPage} from './pages/contributing/contributing.page';
-import {V0_3_0Page} from './pages/v0.3.0/v0_3_0-page';
-import {V0_3_1Page} from './pages/v0.3.1/v0_3_1-page';
+import {
+  WrongRouteComponent,
+} from './pages/wrong-route/wrong-route.component';
 
 export const routes: Routes = [
   {path: '', component: HomePage},
   {path: 'getting-started', component: GettingStartedPage},
   {
     path: 'documentation', component: DocumentationPage, children: [
-      {path: '', redirectTo: '0.3.0', pathMatch: 'full'},
-      {
+      {path: '', redirectTo: '0.3.0', pathMatch: 'full'}, {
         path: '0.3.1',
         component: V0_3_1Page,
         children: V0_3_1Page.sidebarLayout,
-      },
-      {
+      }, {
         path: '0.3.0',
         component: V0_3_0Page,
         children: V0_3_0Page.sidebarLayout,
       }],
   },
   {path: 'examples', component: ExamplesPage},
-  {path: 'contributing', component: ContributingPage}];
+  {path: 'contributing', component: ContributingPage},
+  {path: '**', component: WrongRouteComponent}];
---
+++ src/app/pages/wrong-route/wrong-route.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wrong-route.component',
+  imports: [],
+  templateUrl: './wrong-route.component.html',
+  styleUrl: './wrong-route.component.css'
+})
+export class WrongRouteComponent {
+
+}
