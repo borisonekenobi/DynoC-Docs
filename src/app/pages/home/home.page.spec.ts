640cd08fca1c2f504e2c2bb73402d42718ba4c47
Boris Vasilev <bvasilev@uwo.ca>
2025-10-10 23:17:09 -0400

    Fix tests

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,6 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomePage} from './home.page';
+import {provideRouter} from '@angular/router';
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -8,7 +9,7 @@ describe('HomePage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomePage],
+      imports: [HomePage], providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
--- src/app/pages/v0.3.0/v0_3_0-page.spec.ts
+++ src/app/pages/v0.3.0/v0_3_0-page.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { V0_3_0Page } from './v0_3_0-page';
+import {V0_3_0Page} from './v0_3_0-page';
+import {provideRouter} from '@angular/router';
 
 describe('V030Page', () => {
   let component: V0_3_0Page;
@@ -8,9 +9,8 @@ describe('V030Page', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [V0_3_0Page]
-    })
-    .compileComponents();
+      imports: [V0_3_0Page], providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(V0_3_0Page);
     component = fixture.componentInstance;
