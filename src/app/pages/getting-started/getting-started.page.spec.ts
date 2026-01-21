b6ff69117466b5eb1c905c58b5e12d86c9981b81
Boris Vasilev <bvasilev@uwo.ca>
2026-01-20 23:32:56 -0500

    Update links in getting started and version pages to use routerLink for navigation

--- src/app/pages/getting-started/getting-started.page.spec.ts
+++ src/app/pages/getting-started/getting-started.page.spec.ts
@@ -1,4 +1,5 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {provideRouter} from '@angular/router';
 
 import {GettingStartedPage} from './getting-started.page';
 
@@ -9,6 +10,7 @@ describe('GettingStartedPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [GettingStartedPage],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GettingStartedPage);
--- src/app/pages/getting-started/getting-started.page.ts
+++ src/app/pages/getting-started/getting-started.page.ts
@@ -3,12 +3,15 @@ import {
   PrintLineComponent
 } from '../../components/code/function/print-line/print-line.component';
 import {StringComponent} from '../../components/code/string/string.component';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-getting-started',
   imports: [
     PrintLineComponent,
     StringComponent,
+    RouterLinkActive,
+    RouterLink,
   ],
   templateUrl: './getting-started.page.html',
   styleUrl: './getting-started.page.css',
